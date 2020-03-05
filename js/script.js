055b7be28d7a8523a908c03a5855ea2d5ebed994
Kapitonov Roman <kapitroman@yandex.ru>
2020-03-06 00:42:14 +0300

    замена иконок

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ window.onload = init2;
   function init2() { 
 
   var items = document.querySelectorAll(".item");
-  for (var i = 0; i < items.length; i++) {	
-	items[i].style.backgroundColor = "red";     
+  for (var i = 0; i < items.length; i++) {	     
     items[i].style.height = items[i].offsetHeight + "px";  
   };
 
